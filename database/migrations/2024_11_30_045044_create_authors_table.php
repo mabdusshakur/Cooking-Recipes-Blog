fd7b4b826117f2d707d13dbe26f98b49f5d3ee47
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-12-01 18:35:58 +0600

    Update, author column

--- database/migrations/2024_11_30_045044_create_authors_table.php
+++ database/migrations/2024_11_30_045044_create_authors_table.php
@@ -14,14 +14,14 @@ class CreateAuthorsTable extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->text('mini_bio');
-            $table->longText('main_bio');
-            $table->string('signature');
-            $table->string('profile_title');
+            $table->text('mini_bio')->nullable();
+            $table->longText('main_bio')->nullable();
+            $table->string('signature')->nullable();
+            $table->string('profile_title')->nullable();
             $table->string('main_image')->nullable();
-            $table->string('main_header');
-            $table->string('mini_header');
+            $table->string('main_header')->nullable();
+            $table->string('mini_header')->nullable();
+            $table->foreignId('user_id')->constrained('users')->restrictOnDelete()->cascadeOnUpdate();
             $table->boolean('is_active')->default(false);
             $table->boolean('is_deleted')->default(false);
             $table->timestamps();
