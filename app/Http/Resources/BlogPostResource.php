28a167c71e446199e0f38fcd08132cd904818cfc
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-12-02 17:18:23 +0600

    Update, BlogPost resource

--- app/Http/Resources/BlogPostResource.php
+++ app/Http/Resources/BlogPostResource.php
@@ -14,6 +14,17 @@ class BlogPostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'main_image' => $this->main_image,
+            'author_id' => new AuthorResource($this->whenLoaded('author')),
+            'category' => new BlogPostCategoryResource($this->whenLoaded('blogCategory')),
+            'is_active' => $this->is_active,
+            'is_deleted' => $this->is_deleted,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
