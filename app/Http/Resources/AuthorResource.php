14eedbed9fd6d47ba3f8dcca65870f805b39a8b2
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-12-02 13:49:24 +0600

    Add, Resource

---
+++ app/Http/Resources/AuthorResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'mini_bio' => $this->mini_bio,
+            'main_bio' => $this->main_bio,
+            'signature' => $this->signature,
+            'profile_title' => $this->profile_title,
+            'main_image' => $this->main_image,
+            'recipes' => RecipeResource::collection($this->recipes),
+            'blogs' => BlogPostResource::collection($this->blogPosts),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
