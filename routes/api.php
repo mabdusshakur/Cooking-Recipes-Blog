318d3f097763e70b078dc77266c96becbd30577c
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-12-02 13:50:00 +0600

    Add, Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,50 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\AuthorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'v1'], function () {
+   
+    // Auth Routes
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
+        Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+
+    Route::group(['middleware' => 'api'], function () {
+        // Auth Routes
+        Route::post('auth/logout', [AuthController::class, 'logout']);
+
+        // User Middleware
+        Route::group(['prefix' => 'user', 'middleware' => 'CheckRole:user'], function () {
+            // Apply for author
+            Route::post('/apply', [AuthorController::class, 'apply']); // from user dashboard separate application button
+
+            // User Profile
+            Route::get('/profile', [AuthController::class, 'profile']);
+        });
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
-Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
-Route::post('/login', [AuthController::class, 'login']);
+        // Author Middleware
+        Route::group(['prefix' => 'author', 'middleware' => 'CheckRole:author'], function () {
+            // Update Author Profile
+            Route::put('/', [AuthorController::class, 'update']);
+
+            // Author Profile
+            Route::get('/profile', [AuthorController::class, 'getCurrentAuthorProfile']);
+        });
+       
+        // Admin Middleware
+        Route::group(['prefix' => 'admin', 'middleware' => 'CheckRole:admin'], function () {
+            // Author (Show All, Create, Delete)Routes
+            Route::apiResource('/author', AuthorController::class)->only('index', 'store', 'destroy');
+
+            // Author (Approve) Route
+            Route::patch('/author/approve', [AuthorController::class, 'approveAuthor']);
+        });
+    });
+});
+
