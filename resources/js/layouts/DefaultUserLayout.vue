a41f72c35aab7464f76328aff64827deb0831e6e
Aisha-Moon <aishaaktermoon2@gmail.com>
2024-12-05 14:01:42 +0600

    Added assets for User Dashboard

---
+++ resources/js/layouts/DefaultUserLayout.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+      <h1>Default Layout</h1>
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DefaultLayout',
+    mounted() {
+      // Load CSS files
+      this.loadCSS('/user/assets/css/animate.css');
+      this.loadCSS('/user/assets/css/bootstrap.min.css');
+      this.loadCSS('/user/assets/css/icofont.min.css');
+      this.loadCSS('/user/assets/css/lightcase.css');
+      this.loadCSS('/user/assets/css/swiper.min.css');
+      this.loadCSS('/user/assets/css/style.css');
+  
+      // Load JS files
+      this.loadJS('/user/assets/js/jquery.js');
+      this.loadJS('/user/assets/js/waypoints.min.js');
+      this.loadJS('/user/assets/js/bootstrap.min.js');
+      this.loadJS('/user/assets/js/isotope.pkgd.min.js');
+      this.loadJS('/user/assets/js/wow.min.js');
+      this.loadJS('/user/assets/js/swiper.min.js');
+      this.loadJS('/user/assets/js/lightcase.js');
+      this.loadJS('/user/assets/js/jquery.counterup.min.js');
+      this.loadJS('/user/assets/js/functions.js');
+      this.loadJS('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', true);
+  
+      // Initialize Swiper
+      this.initSwiper();
+    },
+    methods: {
+      loadCSS(href) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+        document.head.appendChild(link);
+      },
+      loadJS(src, isExternal = false) {
+        const script = document.createElement('script');
+        script.src = src;
+        if (isExternal) {
+          script.crossOrigin = 'anonymous';
+        }
+        document.body.appendChild(script);
+      },
+      initSwiper() {
+        const interval = setInterval(() => {
+          if (typeof Swiper !== 'undefined') {
+            clearInterval(interval);
+            new Swiper('.featured-swiper', {
+              slidesPerView: 1,
+              spaceBetween: 30,
+              centeredSlides: false,
+              loop: true,
+              autoplay: {
+                delay: 1200,
+                disableOnInteraction: false,
+              },
+              pagination: {
+                el: '.featured-swiper-pagination',
+                clickable: true,
+              },
+            });
+          }
+        }, 500);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Custom styles (optional) */
+  </style>
+  
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/pages/HomePage.vue';
 import About from '../components/pages/AboutPage.vue';
 import Blog from '../components/pages/BlogPage.vue';
-import DefaultLayout from '../layouts/DefaultLayout.vue';
+import DefaultUserLayout from '../layouts/DefaultUserLayout.vue';
 import DashboardLayout from '../layouts/DashboardLayout.vue';
 
 const routes = [
     {
         path: '/',
-        component: DefaultLayout,
+        component: DefaultUserLayout,
         children: [
             {
                 path: '/',
