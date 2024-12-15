6e3fdc1aaee44d0babbcf45ed3ce08e7c64ee5ff
Aisha-Moon <aishaaktermoon2@gmail.com>
2024-12-15 21:46:18 +0600

    modify recipe

--- resources/views/user/partials/_popularRecipe.blade.php
+++ resources/views/user/partials/_popularRecipe.blade.php
@@ -24,66 +24,69 @@
         </div>
     </div>
 </section>
-
+@section('scripts')
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            fetchRecipesAndCategories();
+        document.addEventListener('DOMContentLoaded',async function() {
+            await fetchCategories();
+            await fetchRecipes();
         });
 
-        function fetchRecipesAndCategories() {
-            const categoryListContainer = document.getElementById('category-list');
-            const recipeListContainer = document.getElementById('recipe-list');
+        async function fetchCategories() {
+    const categoryListContainer = document.getElementById('category-list');
+    categoryListContainer.innerHTML = ''; 
 
-            categoryListContainer.innerHTML = '';
-            recipeListContainer.innerHTML = '';
+    try {
+        const response = await axios.get('{{ url('/api/v1/recipe-category') }}');
+        const data = response.data[0];
+        console.log(data);
 
-            axios.get('{{ url('/api/v1/recipe-category') }}')
-                .then(function(response) {
-                    const data = response.data[0];
-                    console.log(data);
-
-                    if (data && data.length > 0) {
-                        data.forEach(function(category) {
-                            const categoryName = category.name ? category.name : 'Unknown';
-                            const categoryHTML = `
-                <div class="swiper-slide">
-                    <div class="food-item">
-                        <div class="food-thumb">
-                            <a href="#"><img src="default-image.jpg" alt="${categoryName}"></a>
-                        </div>
-                        <div class="food-content">
-                            <a href="#">${categoryName}</a> 
+        if (data && data.length > 0) {
+            data.forEach(category => {
+                const categoryName = category.name ? category.name : 'Unknown';
+                const categoryHTML = `
+                    <div class="swiper-slide">
+                        <div class="food-item">
+                            <div class="food-thumb">
+                                <a href="#"><img src="default-image.jpg" alt="${categoryName}"></a>
+                            </div>
+                            <div class="food-content">
+                                <a href="#">${categoryName}</a> 
+                            </div>
                         </div>
                     </div>
-                </div>
                 `;
-                            categoryListContainer.innerHTML += categoryHTML;
-                        });
+                categoryListContainer.innerHTML += categoryHTML;
+            });
 
+            // Swiper initialization
+            new Swiper(".food-slider", {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                autoplay: {
+                    delay: 3000,
+                    disableOnInteraction: false,
+                },
+            });
+        } else {
+            categoryListContainer.innerHTML = '<p>No categories available at this time.</p>';
+        }
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        categoryListContainer.innerHTML = '<p>Unable to load categories. Please try again later.</p>';
+    }
+}
 
+async function fetchRecipes() {
+    const recipeListContainer = document.getElementById('recipe-list');
+    recipeListContainer.innerHTML = ''; 
 
-                        // Swiper initialization
-                        var swiper = new Swiper(".food-slider", {
-                            slidesPerView: 3,
-                            spaceBetween: 10,
-                            autoplay: {
-                                delay: 3000,
-                                disableOnInteraction: false,
-                            },
-                        });
-                    }
-                })
-                .catch(function(error) {
-                    console.error('Error fetching categories:', error);
-                });
+    try {
+        const response = await axios.get('{{ url('/api/v1/recipes') }}');
+        const data = response.data;
 
-
-            axios.get('{{ url('/api/v1/recipes') }}')
-                .then(function(response) {
-                    const data = response.data;
-                    if (data.data && data.data.length > 0) {
-                        data.data.forEach(function(recipe) {
-                            const recipeHTML = `
+        if (data.data && data.data.length > 0) {
+            data.data.forEach(recipe => {
+                const recipeHTML = `
                     <div class="col-xl-4 col-md-6 col-12">
                         <div class="p-food-item">
                             <div class="p-food-inner">
@@ -97,25 +100,21 @@
                                     <h6><a href="#">${recipe.title}</a></h6>
                                     <p>${recipe.short_description}</p>
                                     <div class="p-food-group">
-<a href="{{ url('/single-recipe') }}/${recipe.id}" class="food-btn">
-    <span>View Recipe</span>
-</a>
+                                        <a href="{{ url('/single-recipe') }}/${recipe.id}" class="food-btn">
+                                            <span>View Recipe</span>
+                                        </a>
                                     </div>
                                     <div class="d-flex my-2 gap-2 text-dark justify-content-center">
                                         <div class="left">
                                             <i class="icofont-stopwatch"></i>
                                             ${recipe.prepare_time || 'N/A'}
                                         </div>
-
-                                                <div class="vr"></div>
-
-                                                <div class="right">
-                                                    <i class="icofont-food-basket"></i>
-                                                    ${recipe.difficulty || 'Unknown'}
-                                                </div>
-
-                                                <div class="vr"></div>
-
+                                        <div class="vr"></div>
+                                        <div class="right">
+                                            <i class="icofont-food-basket"></i>
+                                            ${recipe.difficulty || 'Unknown'}
+                                        </div>
+                                        <div class="vr"></div>
                                         <div class="middle">
                                             <i class="icofont-spoon-and-fork"></i>
                                             ${recipe.serving || 'N/A'} SERVES
@@ -126,13 +125,16 @@
                         </div>
                     </div>
                 `;
-                            recipeListContainer.innerHTML += recipeHTML;
-                        });
-                    }
-                })
-                .catch(function(error) {
-                    console.error('Error fetching recipes:', error);
-                });
+                recipeListContainer.innerHTML += recipeHTML;
+            });
+        } else {
+            recipeListContainer.innerHTML = '<p>No recipes available at this time.</p>';
         }
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        recipeListContainer.innerHTML = '<p>Unable to load recipes. Please try again later.</p>';
+    }
+}
+
     </script>
 @endsection
